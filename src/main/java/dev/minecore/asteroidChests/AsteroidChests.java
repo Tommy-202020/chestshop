bbd306ae3913e635ec72ce6dcbd00b9208814efc
Philip Hanna <philiphanna7777@gmail.com>
2026-05-14 20:50:10 -0700

    first commit

---
+++ src/main/java/dev/minecore/asteroidChests/AsteroidChests.java
@@ -0,0 +1,16 @@
+package dev.minecore.asteroidChests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AsteroidChests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
